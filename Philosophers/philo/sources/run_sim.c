7ed993c79f6f111fff6a155bfac89b45d0fa13f7
Rui Afonso Alves E Oliveira Martins <martins.ruiafonso@gmail.com>
2025-02-10 19:48:37 +0000

    Philosophers and updated libft Makefile

--- Philosophers/philo/sources/run_sim.c
+++ Philosophers/philo/sources/run_sim.c
@@ -6,7 +6,7 @@
 /*   By: ralves-e <ralves-e@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 16:44:49 by ralves-e          #+#    #+#             */
-/*   Updated: 2025/02/08 14:48:07 by ralves-e         ###   ########.fr       */
+/*   Updated: 2025/02/08 15:37:57 by ralves-e         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void	run_sim(t_args *args, t_philo *philos)
 		pthread_mutex_init(&(philos[i].last_eat_mutex), NULL);
 		if (pthread_create(&(philos[i].philo), NULL, &routine, &philos[i]))
 			exit_function(args, &philos);
-		ft_sleep(10);
+		ft_sleep(7);
 	}
 	return ;
 }
